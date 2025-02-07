19b3999398d4e705aa7fe883a48cc25ed01a8e0b
Michael Fox <mfox@amplify.security>
2025-02-07 14:24:28 -0700

    troubleshooting vuln fix

--- routes/log.ts
+++ routes/log.ts
@@ -2,11 +2,9 @@ import { Request, Response, NextFunction } from 'express'
 
 module.exports = function logInsecure() {
     return (req: Request, res: Response, next: NextFunction) => {
-        // let file: any = req.query.file ? req.query.file : ""
-        // let target: any = req.query.target ? req.query.target : ""
+        let file: any = req.query.file ? req.query.file : ""
+        let target: any = req.query.target ? req.query.target : ""
         // console.log(`${file} target: `, JSON.stringify(target, null, 2));
-        const logMessage = `${req.query.file ? req.query.file : ""} target: `;
-        let target: any = req.query.target ? req.query.target : "";
-        console.log(logMessage, JSON.stringify(target, null, 2));
+        console.log('File: %s, Target: %s', file, JSON.stringify(target, null, 2));
     }
 }
