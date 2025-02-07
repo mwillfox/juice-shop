caa49cb2f590edbd2dbb969462e9c64cef3d4033
Michael Fox <mfox@amplify.security>
2025-02-07 14:18:43 -0700

    commit amplify fix

--- routes/log.ts
+++ routes/log.ts
@@ -2,8 +2,11 @@ import { Request, Response, NextFunction } from 'express'
 
 module.exports = function logInsecure() {
     return (req: Request, res: Response, next: NextFunction) => {
-        let file: any = req.query.file ? req.query.file : ""
-        let target: any = req.query.target ? req.query.target : ""
-        console.log(`${file} target: `, JSON.stringify(target, null, 2));
+        // let file: any = req.query.file ? req.query.file : ""
+        // let target: any = req.query.target ? req.query.target : ""
+        // console.log(`${file} target: `, JSON.stringify(target, null, 2));
+        const logMessage = `${req.query.file ? req.query.file : ""} target: `;
+        let target: any = req.query.target ? req.query.target : "";
+        console.log(logMessage, JSON.stringify(target, null, 2));
     }
 }
