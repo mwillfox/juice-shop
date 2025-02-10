73984997aab738dddbb0cf5aced6134abd0dcb73
Michael Fox <mfox@amplify.security>
2025-02-10 16:19:23 -0700

    Revert "Accepted Amplify fix"
    This reverts commit 7557aa066122248e5ab402385e41efc28b974051.

--- routes/log.ts
+++ routes/log.ts
@@ -4,6 +4,6 @@ module.exports = function logInsecure() {
     return (req: Request, res: Response, next: NextFunction) => {
         let file: any = req.query.file ? req.query.file : ""
         let target: any = req.query.target ? req.query.target : ""
-        console.log('File: %s, Target: ', file, JSON.stringify(target, null, 2))
+        console.log(`${file} target: `, JSON.stringify(target, null, 2));
     }
 }
