7b63329ed2bb20ee390404738f8317e059875c1a
Michael Fox <m.will.fox@gmail.com>
2025-03-17 16:06:48 -0600

    Removed routes/log.ts

--- routes/log.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express'
-
-module.exports = function logInsecure() {
-    return (req: Request, res: Response, next: NextFunction) => {
-        let file: any = req.query.file ? req.query.file : ""
-        let target: any = req.query.target ? req.query.target : ""
-        console.log(`${file} target: `, JSON.stringify(target, null, 2));
-    }
-}
